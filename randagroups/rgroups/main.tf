efb805cfe812c21066dc75d00c6843a0a0d594a7
Tim Robinson <timroster@gmail.com>
2021-04-02 15:46:05 -0700

    retrieve default rg from data

--- randagroups/rgroups/main.tf
+++ randagroups/rgroups/main.tf
@@ -6,6 +6,6 @@ resource "ibm_resource_group" "resourceGroup" {
   name = var.resource_group_name
 }
 
-resource "ibm_resource_group" "resourceGroupDefault" {
-  name = var.default_resource_group_name
+data "ibm_resource_group" "resourceGroupDefault" {
+  is_default = "true"
 }
