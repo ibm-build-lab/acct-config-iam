4f6f952196feb9ada6095928339eed0bd2a6eceb
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-05-24 19:38:54 -0500

    Update main.tf

--- randagroups/agroups/main.tf
+++ randagroups/agroups/main.tf
@@ -189,12 +189,14 @@ resource "ibm_iam_access_group_policy" "sat_service_policy" {
   }
 }
 
+/*
 resource "ibm_iam_access_group_policy" "sat_link_policy" {
   access_group_id = ibm_iam_access_group.sat_access_group.id
-  roles =  ["Administrator"]
+  roles =  ["Satellite Link Administrator"]
   resources  {
     service = "satellite"
     resource_type = "link"
   }
 }
+*/
 
