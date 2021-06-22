0d4db0cc22e5cbb4d89e23e510f3fc23283cce79
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-06-22 15:18:27 -0500

    Update create_keys.sh

--- create_keys.sh
+++ create_keys.sh
@@ -4,5 +4,5 @@ ibmcloud target -g $RESOURCE_GROUP
 echo "What is region 'ibmcloud regions': "
 read REGION
 
-# ibmcloud ks api-key reset --region $REGION
-ibmcloud ks credential set classic --infrastructure-api-key partner-sandbox-api-key --infrastructure-username $SERVICE_ID --region $REGION
+ibmcloud ks api-key reset --region $REGION
+# ibmcloud ks credential set classic --infrastructure-api-key partner-sandbox-api-key --infrastructure-username $SERVICE_ID --region $REGION
