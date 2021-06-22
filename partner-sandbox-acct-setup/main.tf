0fee872dc33927022f24860afb5ae419b55eb46b
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-06-22 13:38:21 -0500

    Update main.tf

--- partner-sandbox-acct-setup/main.tf
+++ partner-sandbox-acct-setup/main.tf
@@ -4,7 +4,7 @@ provider "ibm" {
   version = "~> 1.12"
 }
 
-module "resource-groups" {
+module "resource-group" {
   source = "../modules/resource-group"
 
   resource_group_name = var.resource_group_name
@@ -13,20 +13,20 @@ module "resource-groups" {
 module "access-groups" {
   source = "../modules/access-groups"
 
-  resource_group_name       = module.resource-groups.resource_group_name
-  resource_group_id         = module.resource-groups.resource_group_id
-  default_resource_group_id = module.resource-groups.default_resource_group_id
+  resource_group_name       = module.resource-group.resource_group_name
+  resource_group_id         = module.resource-group.resource_group_id
+  default_resource_group_id = module.resource-group.default_resource_group_id
   admins_access_group_name  = var.admins_access_group_name
   users_access_group_name   = var.users_access_group_name
   sat_access_group_name     = var.sat_access_group_name
 }
 
-module "account" {
+module "account-setup" {
   source = "../modules/account-setup"
 
   at_plan                   = var.at_plan
   region                    = var.region
-  resource_group_id         = module.resource-groups.resource_group_id
+  resource_group_id         = module.resource-group.resource_group_id
   admins_access_group_id    = module.access-groups.admins_access_group_id
   service_id_name           = var.service_id_name
 }
