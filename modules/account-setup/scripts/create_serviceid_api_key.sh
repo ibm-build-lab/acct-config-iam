e1e511ae9da1ee3fa6e3a35c58d74062438e596c
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-06-29 17:59:51 -0500

    Update create_serviceid_api_key.sh

--- modules/account-setup/scripts/create_serviceid_api_key.sh
+++ modules/account-setup/scripts/create_serviceid_api_key.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-ibmcloud iam service-api-key-create partner-sandbox-api-key $SERVICE_ID
+ibmcloud iam service-api-key-create partner-sandbox-api-key $SERVICEID
 
-#export SERVICEID_API_KEY=$(ibmcloud iam service-api-key-create partner-sandbox-api-key $SERVICE_ID | awk '/API Key/{print $3}') 
+#export SERVICEID_API_KEY=$(ibmcloud iam service-api-key-create partner-sandbox-api-key $SERVICEID | awk '/API Key/{print $3}') 
 #echo "Service ID API Key: $SERVICEID_API_KEY"
 # ibmcloud login --apikey $SERVICEID_API_KEY -g $RESOURCE_GROUP
 # ibmcloud ks api-key reset --region $REGION
