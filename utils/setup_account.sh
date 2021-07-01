ca2aaf955e83dd5a6b7bb2bf0c9d85824ab4cd16
Ann Umberhocker <ann.umberhocker@ibm.com>
2021-07-01 12:52:39 -0500

    clean up

--- utils/setup_account.sh
+++ utils/setup_account.sh
@@ -13,17 +13,17 @@ fi
 mkdir -p ./logs
 echo "Creating workspace for resource group"
 ibmcloud schematics workspace new --file ../templates/${RESOURCE_GROUP}.json --json > ./logs/${RESOURCE_GROUP}.json
-echo "Sleeping for 15 seconds"
+#echo "Sleeping for 15 seconds"
 sleep 15
 WORKSPACE_ID=$(jq -r '.id' ./logs/${RESOURCE_GROUP}.json) 
 
 echo "Planning workspace"
 ibmcloud schematics plan --id $WORKSPACE_ID     
-echo "Sleeping for 2 minutes"
-sleep 120
+#echo "Sleeping for 1 minute"
+sleep 60
 
 echo "Applying workspace"
 ibmcloud schematics apply --id $WORKSPACE_ID --force 
-echo "Sleeping for 2 minutes"
-sleep 120
+#echo "Sleeping for 2 minutes"
+#sleep 120
 
