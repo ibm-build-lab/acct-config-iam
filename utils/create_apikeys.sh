c1e9093f4c8c95026ab587fc9d1045141b041af8
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-09-22 13:09:51 -0500

    added command to create api-key for Admin user first

--- utils/create_apikeys.sh
+++ utils/create_apikeys.sh
@@ -4,6 +4,7 @@ RESOURCE_GROUP=$1
 REGION=$2
 SERVICE_ID=partner-sandbox-admin-id
 ibmcloud login -sso -g $RESOURCE_GROUP
+ibmcloud ks api-key reset --region $REGION
 export SERVICEID_API_KEY=$(ibmcloud iam service-api-key-create partner-sandbox-api-key $SERVICE_ID --file serviceid-api-key.json -d "API key for partner sandbox service ID"| awk '/API Key/{print $3}') 
 echo "Service ID API Key: $SERVICEID_API_KEY"
 ibmcloud login --apikey $SERVICEID_API_KEY -g $RESOURCE_GROUP
