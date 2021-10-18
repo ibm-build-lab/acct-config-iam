9fbe9ba4c3b3896801466d0e7f622910dc83f1e2
Ann Umberhocker <38887336+annumberhocker@users.noreply.github.com>
2021-10-18 17:07:52 -0500

    changed role to Satellite Link Administrator

--- modules/access-groups/main.tf
+++ modules/access-groups/main.tf
@@ -207,7 +207,7 @@ resource "ibm_iam_access_group_policy" "sat_service_policy" {
 
 resource "ibm_iam_access_group_policy" "sat_link_policy" {
   access_group_id = ibm_iam_access_group.sat_access_group.id
-  roles =  ["Satellite Link Source and Endpoint Controller"]
+  roles =  ["Satellite Link Administrator"]
   resources  {
     service = "satellite"
     resource_type = "link"
